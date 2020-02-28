d2ee571cca799665b212eb7232d4c82017dc10d9
Dewy REDACTED <dljcoolguy@gmail.com>
2020-02-28 20:12:12 +0000

    I exist!
    Edited mods.toml so that it works, and added the mod's main class.

--- src/main/java/dev/dewy/paracosm/Paracosm.java
+++ src/main/java/dev/dewy/paracosm/Paracosm.java
@@ -1,6 +1,45 @@
 package dev.dewy.paracosm;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+/**
+ * Paracosm's main class. This is where it all begins, again.
+ *
+ * @author Dewy
+ */
+@Mod(Paracosm.MODID)
 public class Paracosm
 {
+    // Constants
+    public static final String MODID = "paracosm";
+    public static final String NAME = "Paracosm";
 
+    public Paracosm()
+    {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        eventBus.register(this);
+    }
+
+    // Event Handlers
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void setupCommon(FMLCommonSetupEvent event)
+    {
+        // Something, for now...
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void setupClient(FMLClientSetupEvent event)
+    {
+        // Nothing, for now...
+    }
 }
